678f00feee42799f16959184ff02bbfd0c071b4f
root <root@DESKTOP-JNMSK2O>
2024-01-28 12:09:29 +0000

    feat: force push after fixes

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'section-4': "url('../src/assets/art.svg')",
+        'talk-cover': "url('../src/assets/talk-cover.svg')",
+        'signup-section': "url('../src/assets/signup-bg.svg')",
+        'gradient-1': "linear-gradient(180deg, rgba(255, 255, 255, 0.00) 0%, #F4F0FF 100%)"
+      },
+      fontFamily: {
+        Satoshi: ['Satoshi', 'sans-serif'],
+        Inter: ['Inter', 'sans-serif'],
+      },
+
+    },
+  },
+  plugins: [],
+}
+
+
+
+
+
+
+
+
+
